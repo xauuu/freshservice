a2b0a631e3b0633d4eb5d160465bc60902db3bbc
Quang Dat <qdatqb@gmail.com>
2023-10-19 21:37:33 +0700

    .

--- Freskservice/Process Approval/server/lib/utils.js
+++ Freskservice/Process Approval/server/lib/utils.js
@@ -1,77 +1,83 @@
 const nodemailer = require("nodemailer");
-const moment = require('moment');
+const moment = require("moment");
 
 const transporter = nodemailer.createTransport({
-    host: "email-smtp.ap-southeast-1.amazonaws.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "AKIAR6X2FCDUCAUH5CF5",
-        pass: "BB6+9lWVXwURGcp6qKmfNb7ZXvff8mef24zAXyFm+mB+"
-    }
+  host: "email-smtp.ap-southeast-1.amazonaws.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "AKIAR6X2FCDUCAUH5CF5",
+    pass: "BB6+9lWVXwURGcp6qKmfNb7ZXvff8mef24zAXyFm+mB+"
+  }
 });
 
-async function sendMail(subject, html, to, attachments = []) {
-    await transporter.sendMail({
-        from: "Truvisor Freshservice <info@truebpm.vn>",
-        to,
-        subject,
-        html,
-        attachments
-    });
+function sendMail(subject, html, to, attachments = []) {
+  const options = {
+    from: "Truvisor Freshservice <info@truebpm.vn>",
+    to,
+    subject,
+    html,
+    attachments
+  };
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent: " + info.response);
+    }
+  });
 }
-
 const formattedApprovals = (approvals) => {
-    return approvals.map(approval => {
-        let approvalStatus;
-        if (approval.approval_status.id === 0) {
-            approvalStatus = "Requested";
-        } else if (approval.approval_status.id === 1) {
-            approvalStatus = "Approved";
-        } else if (approval.approval_status.id === 2) {
-            approvalStatus = "Rejected";
-        } else {
-            approvalStatus = "Unknown";
-        }
+  return approvals.map((approval) => {
+    let approvalStatus;
+    if (approval.approval_status.id === 0) {
+      approvalStatus = "Requested";
+    } else if (approval.approval_status.id === 1) {
+      approvalStatus = "Approved";
+    } else if (approval.approval_status.id === 2) {
+      approvalStatus = "Rejected";
+    } else {
+      approvalStatus = "Unknown";
+    }
 
-        return {
-            approval: {
-                approval_status: approval.approval_status.id,
-                approvable_type: "Helpdesk::Ticket",
-                remark: approval.latest_remark ? [[approval.approval_status.id, approval.latest_remark, Date.now()]] : [],
-                created_at: approval.created_at,
-                updated_at: approval.updated_at,
-                level_id: approval.level,
-                approval_type: approval.approval_type,
-                requestedOn: approvalStatus + " on ",
-                name: approval.approver_name,
-                avatar_url: `https://trusisor.myfreshworks.com/api/v2/users/${approval.user_id}/image?variant=SMALL`
-            }
-        };
-    });
+    return {
+      approval: {
+        approval_status: approval.approval_status.id,
+        approvable_type: "Helpdesk::Ticket",
+        remark: approval.latest_remark ? [[approval.approval_status.id, approval.latest_remark, Date.now()]] : [],
+        created_at: approval.created_at,
+        updated_at: approval.updated_at,
+        level_id: approval.level,
+        approval_type: approval.approval_type,
+        requestedOn: approvalStatus + " on ",
+        name: approval.approver_name,
+        avatar_url: `https://trusisor.myfreshworks.com/api/v2/users/${approval.user_id}/image?variant=SMALL`
+      }
+    };
+  });
 };
 
 function replaceHtmlContent(html, replacements) {
-    let modifiedHtml = html;
+  let modifiedHtml = html;
 
-    for (const key in replacements) {
-        if (replacements.hasOwnProperty(key)) {
-            const value = replacements[key];
-            const regex = new RegExp(`{{${key}}}`, 'g');
-            modifiedHtml = modifiedHtml.replace(regex, value);
-        }
+  for (const key in replacements) {
+    if (replacements.hasOwnProperty(key)) {
+      const value = replacements[key];
+      const regex = new RegExp(`{{${key}}}`, "g");
+      modifiedHtml = modifiedHtml.replace(regex, value);
     }
+  }
 
-    return modifiedHtml;
+  return modifiedHtml;
 }
 
 function formatDate(date) {
-    return moment(date).format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]')
+  return moment(date).format("YYYY-MM-DD[T]HH:mm:ss.SSS[Z]");
 }
 
 exports = {
-    sendMail,
-    formattedApprovals,
-    replaceHtmlContent,
-    formatDate
-}
+  sendMail,
+  formattedApprovals,
+  replaceHtmlContent,
+  formatDate
+};
--- Freskservice/Process Approval/server/server.js
+++ Freskservice/Process Approval/server/server.js
@@ -1,12 +1,11 @@
 const Handlebars = require("handlebars");
 
-const fetch = require('./lib/fetch');
-const utils = require('./lib/utils');
+const fetch = require("./lib/fetch");
+const utils = require("./lib/utils");
 
-const SERVICE_REQUESTS = ["Service Request", "Request"]
+const SERVICE_REQUESTS = ["Service Request", "Request"];
 
 exports = {
-
   onTicketUpdateHandler: async function (args) {
     const ticket = args.data.ticket;
     const changes = args.data.ticket.changes;
@@ -14,36 +13,36 @@ exports = {
       if (!SERVICE_REQUESTS.includes(ticket.type_name) || !(changes.gf_int_01 && changes.gf_int_01.length > 0)) return;
       // utils.sendMail("SR Update", JSON.stringify(ticket), "qdatqb@gmail.com")
 
-      const requestedItems = await fetch.getRequestedItems(ticket.id)
-      console.log(requestedItems)
-      const serviceCategory = await fetch.getServiceCategory(requestedItems.service_item_id)
+      const requestedItems = await fetch.getRequestedItems(ticket.id);
+      console.log(requestedItems);
+      const serviceCategory = await fetch.getServiceCategory(requestedItems.service_item_id);
       if (!serviceCategory) return;
 
-      console.log("START", ticket.id)
+      console.log("START", ticket.id);
       // const processLog = await fetch.getProcessLog(serviceCategory.app_code, serviceCategory.process_code, ticket.id)
       // console.log("DEBUG: Process Data Log", processLog)
 
       if (changes.gf_int_01[0] < changes.gf_int_01[1] || changes.gf_int_01[1] == 0) {
-        const approvalRule = await fetch.getGroupApprovalRule(serviceCategory.app_code, serviceCategory.process_code, changes.gf_int_01[1])
-        console.log("DEBUG-APPROVAL: Group Approval Rule", approvalRule)
-        if (!approvalRule) return
-        await handleApproval(args, ticket, approvalRule, true, requestedItems)
+        const approvalRule = await fetch.getGroupApprovalRule(serviceCategory.app_code, serviceCategory.process_code, changes.gf_int_01[1]);
+        console.log("DEBUG-APPROVAL: Group Approval Rule", approvalRule);
+        if (!approvalRule) return;
+        await handleApproval(args, ticket, approvalRule, true, requestedItems);
       }
       if (changes.gf_int_01[0] > changes.gf_int_01[1]) {
-        const approvalRule = await fetch.getGroupApprovalRule(serviceCategory.app_code, serviceCategory.process_code, changes.gf_int_01[0])
-        console.log("DEBUG-REJECT: Group Approval Rule", approvalRule)
-        if (!approvalRule) return
-        await handleRejection(args, ticket, approvalRule)
+        const approvalRule = await fetch.getGroupApprovalRule(serviceCategory.app_code, serviceCategory.process_code, changes.gf_int_01[0]);
+        console.log("DEBUG-REJECT: Group Approval Rule", approvalRule);
+        if (!approvalRule) return;
+        await handleRejection(args, ticket, approvalRule);
       }
-      console.log("END", ticket.id)
+      console.log("END", ticket.id);
     } catch (error) {
-      console.log(error)
-      utils.sendMail(`${ticket.id} - Error Update Process Approval`, JSON.stringify(error), "qdatqb@gmail.com")
+      console.log(error);
+      utils.sendMail(`${ticket.id} - Error Update Process Approval`, JSON.stringify(error), "qdatqb@gmail.com");
     }
   },
   onTicketCreateHandler: async function (args) {
     const ticket = args.data.ticket;
-    console.log(ticket.id)
+    console.log(ticket.id);
     /*
     try {
       if (ticket.type_name !== SERVICE_REQUEST) return
@@ -84,66 +83,93 @@ exports = {
     }
     */
   }
-
 };
 
 async function handleApproval(args, ticket, approvalRule, isUpdated = false, requestedItems) {
-  console.log("APPROVAL")
+  console.log("APPROVAL");
+  const placeholder = { ticket, requested_item: requestedItems };
   let requesters = [];
-  let approver = ""
+  let approver = "";
   if (approvalRule.approver == 1) {
     //reporting manager
     const currentRequester = await fetch.getRequester(ticket.requester_id);
     const directRequester = await fetch.getRequester(currentRequester.reporting_manager_id);
     requesters = [directRequester];
-    approver = `Reporting Manager: ${directRequester?.first_name} ${directRequester?.last_name}`
+    approver = `Reporting Manager: ${directRequester?.first_name} ${directRequester?.last_name}`;
   } else if (approvalRule.approver == 2) {
     //department head
-    const department = await fetch.getDepartment(ticket.department_id)
+    const department = await fetch.getDepartment(ticket.department_id);
     if (department.head_user_id) {
       const requester = await fetch.getRequester(department.head_user_id);
-      requesters = [requester]
-      approver = `Head of ${ticket.department_name} Department: ${requester?.first_name} ${requester?.last_name}`
+      requesters = [requester];
+      approver = `Head of ${ticket.department_name} Department: ${requester?.first_name} ${requester?.last_name}`;
     }
   } else if (approvalRule.approver == 3) {
     //selected department
-    if (requestedItems.custom_fields.hasOwnProperty('department') && !!requestedItems.custom_fields.department) {
-      const department = await fetch.getDepartment(requestedItems.custom_fields.department)
+    if (requestedItems.custom_fields.hasOwnProperty("department") && !!requestedItems.custom_fields.department) {
+      const department = await fetch.getDepartment(requestedItems.custom_fields.department);
       if (department.head_user_id) {
         fetch.updateRequestedItem(ticket.id, requestedItems.id, {
-          "custom_fields": {
-            "department_head": department.head_user_id
+          custom_fields: {
+            department_head: department.head_user_id
           }
-        })
+        });
         const requester = await fetch.getRequester(department?.head_user_id);
-        requesters = [requester]
-        approver = `Head of ${department?.name} Department: ${requester?.first_name} ${requester?.last_name}`
+        requesters = [requester];
+        approver = `Head of ${department?.name} Department: ${requester?.first_name} ${requester?.last_name}`;
       }
     }
   } else if (approvalRule.approver == 4) {
     //group
-    const [list, detail] = await Promise.all([fetch.getRequesters(approvalRule.next_approval_group), fetch.getRequesterGroup(approvalRule.next_approval_group)]);
-    requesters = list
-    approver = `Group ${detail.name}`
+    const [list, detail] = await Promise.all([
+      fetch.getRequesters(approvalRule.next_approval_group),
+      fetch.getRequesterGroup(approvalRule.next_approval_group)
+    ]);
+    requesters = list;
+    approver = `Group ${detail.name}`;
   }
-  console.log('DEBUG: Group Approval', requesters)
+  console.log("DEBUG: Group Approval", requesters);
   fetch.updateTicket(ticket.id, {
-    "custom_fields": {
-      "ticket_status": approvalRule.name,
-      "pending_approver": `Pending ${approver}`,
+    custom_fields: {
+      ticket_status: approvalRule.name,
+      pending_approver: `Pending ${approver}`
     }
-  })
+  });
   if (requesters && requesters.length > 0) {
-    await Promise.all(requesters.map(async (item) => {
-      await fetch.requestApproval(ticket.id, item.id, approvalRule.approval_type, `${item.first_name} ${item.last_name}, please approve.`);
-    })).then(async function () {
-      console.log('All requests were successful');
-      if (isUpdated)
-        sendEmailTemplate(approvalRule.approval_email_template_code, args.data, args.data.requester.email)
-      sendEmailTemplate(approvalRule.email_template_code, args.data, requesters.map(r => r.primary_email).join(","))
-    }).catch(function (err) {
-      console.error('An error occurred:', err);
-    });
+    if (Boolean(approvalRule.is_not_send_approval)) {
+      console.log("Is only send email");
+      sendEmailTemplate(
+        approvalRule.email_template_code,
+        placeholder,
+        requesters
+          .map((r) => {
+            return r.primary_email || r.email;
+          })
+          .join(",")
+      );
+      return;
+    }
+    await Promise.all(
+      requesters.map(async (item) => {
+        await fetch.requestApproval(ticket.id, item.id, approvalRule.approval_type, `${item.first_name} ${item.last_name}, please approve.`);
+      })
+    )
+      .then(async function () {
+        console.log("All requests were successful");
+        if (isUpdated) sendEmailTemplate(approvalRule.approval_email_template_code, placeholder, args.data.requester.email);
+        sendEmailTemplate(
+          approvalRule.email_template_code,
+          placeholder,
+          requesters
+            .map((r) => {
+              return r.primary_email || r.email;
+            })
+            .join(",")
+        );
+      })
+      .catch(function (err) {
+        console.error("An error occurred:", err);
+      });
     // const approvals = await fetch.getTicketApprovals(ticket.id)
     // const jsonApprovals = JSON.stringify(utils.formattedApprovals(approvals))
     // const newVisible = processLog.sr_visible_to?.concat(';', requesters.map(r => r.id).join(";"))
@@ -152,15 +178,17 @@ async function handleApproval(args, ticket, approvalRule, isUpdated = false, req
 }
 
 async function handleRejection(args, ticket, approvalRule) {
-  console.log("REJECT")
+  console.log("REJECT");
   const requesters = await fetch.getRequesters(approvalRule.reject_next_approval_group);
-  console.log('DEBUG: Group Approval', requesters)
+  console.log("DEBUG: Group Approval", requesters);
 
   if (requesters && requesters.length > 0) {
-    await Promise.all(requesters.map((item) => {
-      fetch.requestApproval(ticket.id, item.id, approvalRule.approval_type, `${item.first_name} ${item.last_name}, please approve.`);
-    }));
-    sendEmailTemplate(approvalRule.reject_email_template_code, args.data, args.data.requester.email)
+    await Promise.all(
+      requesters.map((item) => {
+        fetch.requestApproval(ticket.id, item.id, approvalRule.approval_type, `${item.first_name} ${item.last_name}, please approve.`);
+      })
+    );
+    sendEmailTemplate(approvalRule.reject_email_template_code, args.data, args.data.requester.email);
     // const approvals = await fetch.getTicketApprovals(ticket.id)
     // const jsonApprovals = JSON.stringify(utils.formattedApprovals(approvals))
     // const newVisible = processLog.sr_visible_to?.concat(';', requesters.map(r => r.id).join(";"))
@@ -169,12 +197,16 @@ async function handleRejection(args, ticket, approvalRule) {
 }
 
 async function sendEmailTemplate(template_code, data, to_list) {
-  if (!template_code) return
-  const emailTemplate = await fetch.getEmailTemplate(template_code)
-  if (!emailTemplate) return
-  const templateBody = Handlebars.compile(emailTemplate.body);
-  const templateSubject = Handlebars.compile(emailTemplate.subject);
-  const body = templateBody(data);
-  const subject = templateSubject(data)
-  utils.sendMail(subject, body, to_list)
+  try {
+    if (!template_code) return;
+    const emailTemplate = await fetch.getEmailTemplate(template_code);
+    if (!emailTemplate) return;
+    const templateBody = Handlebars.compile(emailTemplate.body);
+    const templateSubject = Handlebars.compile(emailTemplate.subject);
+    const body = templateBody(data);
+    const subject = templateSubject(data);
+    utils.sendMail(subject, body, to_list);
+  } catch (error) {
+    console.log("Error Send email template", error);
+  }
 }
